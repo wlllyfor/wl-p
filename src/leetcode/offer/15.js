03686846cf005fb87114bebb316b9e8e26adefe5
wlllyfor <958167224@qq.com>
2020-09-24 19:25:06 +0800

    feat：新增一道leetcode题

--- src/leetcode/offer/15.js
+++ src/leetcode/offer/15.js
@@ -3,9 +3,7 @@
  * @return {number}
  */
 var hammingWeight = function(n) {
-    // console.log(n.toString(2))
     const resArr = (n.toString(2)).match(/1/g)
-    // console.log(resArr)
     if(Array.isArray(resArr)) {
         return resArr.length
     } else {
